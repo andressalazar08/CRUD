2cd3e179bb39d1f0b3f68e251ff73de7b63302e7
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2025-05-20 20:07:11 -0500

    crud listo

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # CRUD
+
+## hacer git clone del proyecto
+```
+pip install requirements.txt
+```
+
+Para ejecutar el proyecto
+```
+flask --app main run --debug
+```
+
+
+
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def crud():
     global id_contador
     
     if request.method=='POST': #Si accedemos a la ruta con datos en el formulario
-        nombre=request.form.get("nombre")
+        nombre=request.form.get("nombre") #guarda en una variable de python lo que el usuario entrega en el form
         correo=request.form.get("correo")
         usuarios.append({"id":id_contador,"nombre_usuario":nombre,"correo_usuario":correo}) # insertando un usuario
         id_contador+=1
@@ -23,12 +23,33 @@ def crud():
             if str(item['id'])==id_eliminar:
                 usuarios.remove(item)
                 break
-        return redirect(url_for("crud")) #Llamar al nombre de la función
-            
-     
+        return redirect(url_for("crud")) #Llamar al nombre de la función    
        
     return render_template("registro.html",usuarios=usuarios) #lista que entregamos al html
 
-
+#Ruta de actualización de los datos del usuario
+@app.route("/update/<int:id>",methods=['GET', 'POST']) #Ruta con parámetros
+def update(id):
+    print(usuarios) #esta es la lista global
+    estudiante_a_editar=''
+    #TODO: identificar el diccionario del usuario con id entregado
+    for diccionario in usuarios:
+        if diccionario['id']==id:
+            estudiante_a_editar=diccionario
+            print("El estudiante a editar es: ", estudiante_a_editar)
+            break
+    
+    if request.method=='POST':
+        #TODO:actualizar el diccionario del estudiante con los datos del formulario
+        estudiante_a_editar['nombre_usuario']=request.form.get('nombre')
+        estudiante_a_editar['correo_usuario']=request.form.get('correo')
+        return redirect(url_for("crud"))
+    
+    #Si despues de recorrer toda la lista, no encontramos el id entregado
+    if estudiante_a_editar=='':
+        return f"no existe el usuario con id: {id}" #Salgo de la función
+    
+    return render_template("editar.html",estudiante_a_editar=estudiante_a_editar)
+    
 if __name__=="main":
 	app.run(debug=True)
